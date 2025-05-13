12cde5762d22f53345a9fa3e7ecae8bcaa24f1fb
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-13 14:27:06 -0300

    style: change header font color and add another icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import logoImg from "../assets/store01.svg";
 
 export function Header() {
   return (
-    <header className="border-b-2 border-[#F5EAEA]">
-      <div className="flex flex-row gap-4 p-6 bg-[#FBF4F4]">
+    <header className="border-b-2 border-gray-200">
+      <div className="flex flex-row gap-4 p-6 bg-gray-50">
         <img src={logoImg} alt="Logo" className="w-12" />
         <div>
-          <h3 className="">Montink Marketplace</h3>
-          <span>A sua loja favorita!</span>
+          <h3 className="text-orange-600">Montink Marketplace</h3>
+          <span className="text-orange-600">A sua loja favorita!</span>
         </div>
       </div>
     </header>
